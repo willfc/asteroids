dc0a01cbcb3946d7defb964df6a76cb50748d23e
willfc <willfc000@gmail.com>
2025-01-08 11:39:04 +0800

    game loop and display

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+       
+        screen.fill(color=(0,0,0))
+        pygame.display.flip()
+
+main()
+
+if __name__ == "main":
+    main()
